9d46558ba8e6d09c1b4278669c9a8499430d6aa8
iamwill123 <williampeiyuan@gmail.com>
2017-06-05 11:08:12 -0400

    updates
    one more time default image update

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 const API_KEY = 'H7CF2IHbEc6QIrMVwb2zfd9VI14HHGAfYax1eHEUsJ4voYuqWF2oWvByUOhERva_';
 
 // Helpers
-var imageErrorMobile = '../img/flightBoston.jpg' || 'http://via.placeholder.com/384x288';
+var imageErrorMobile = 'http://via.placeholder.com/384x288' || '../img/flightBoston.jpg';
 var imageErrorPad = 'http://via.placeholder.com/720x405' || '../img/flightBoston.jpg';
 var imageErrorDesktop = 'http://via.placeholder.com/360x202' || '../img/flightBoston.jpg';
 
